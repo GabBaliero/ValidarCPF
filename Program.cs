6d61e8fb108e720f311fabd21cbcf9a1d830e91e
GabBaliero <gabriel.baliero.oliv@gmail.com>
2025-08-21 22:47:25 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,18 +7,23 @@ public class Program
     private static int _dv2;
     public static void Main()
     {
-        ConverterParaArray(_cpf);
+        (_dv1, _dv2) = Digitos(_cpf);
     }
 
-    static void ConverterParaArray(string cpf)
+    static int VerificarDigitos(string cpf, int VerificarDigito)
     {
         cpf = cpf.Replace(".", "").Replace("-", "");
         int[] cpfArray = cpf.ToCharArray().Select(x => int.Parse(x.ToString())).ToArray();
-        for (int i = 0; i < 9; i++)
+        var index = VerificarDigito == 1 ? 9 : 10;
+        var multiplo = VerificarDigito == 1 ? 11 : 12;
+        for (int i = 0; i < index; i++)
         {
-            cpfArray[i] *= (11 - cpfArray[i]);
+            cpfArray[i] *= (multiplo - cpfArray[i]);
         }
         int sum = cpfArray.Take(9).Sum(c => int.Parse(c.ToString()));
-        _dv1 = (sum % 11) < 2 ? 0 : (11 - sum % 11);
+        VerificarDigito = (sum % 11) < 2 ? 0 : (11 - sum % 11);
+        return VerificarDigito;
     }
+    static (int, int) Digitos(string cpf) => (VerificarDigitos(_cpf, 1), VerificarDigitos(_cpf, 2));
+
 }
