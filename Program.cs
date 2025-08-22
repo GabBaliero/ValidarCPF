453a76239d034bf30b7b6cd6bfea15eee0abb10b
GabBaliero <gabriel.baliero.oliv@gmail.com>
2025-08-21 21:02:52 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+public class Program
+{
+    private static string _cpf = "123.456.789-09";
+    private static int _dv1;
+    private static int _dv2;
+    public static void Main()
+    {
+        ConverterParaArray(_cpf);
+    }
+
+    static void ConverterParaArray(string cpf)
+    {
+        cpf = cpf.Replace(".", "").Replace("-", "");
+        int[] cpfArray = cpf.ToCharArray().Select(x => int.Parse(x.ToString())).ToArray();
+        for (int i = 0; i < 9; i++)
+        {
+            cpfArray[i] *= (11 - cpfArray[i]);
+        }
+        int sum = cpfArray.Take(9).Sum(c => int.Parse(c.ToString()));
+        _dv1 = (sum % 11) < 2 ? 0 : (11 - sum % 11);
+    }
+}
